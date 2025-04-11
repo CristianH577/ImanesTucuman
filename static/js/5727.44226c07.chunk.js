4315202633c82fb0abf6b6df6128b75bd7ee4779
Cristian H. <115647939+CristianH577@users.noreply.github.com>
2025-04-11 15:18:17 -0300

    Updates

---
+++ static/js/5727.44226c07.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkimanestuc_app=self.webpackChunkimanestuc_app||[]).push([[5727,8108],{35727:(e,a,s)=>{s.r(a),s.d(a,{default:()=>n});var n=s(30082).l},30082:(e,a,s)=>{s.d(a,{l:()=>c});var n=s(22555),p=s(53279),r=s(1299),u=s(59914);const c=(0,n.A)((0,n.A)({renderer:u.J},p.W),r.n)}}]);
+//# sourceMappingURL=5727.44226c07.chunk.js.map
